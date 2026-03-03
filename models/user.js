0472a3a44780ae6ea5361f8223d8f518b355410f
Payal Sandeep Gatkal <payalgatkal50@gmail.com>
2026-03-03 11:15:28 +0530

    updated

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const custSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports=mongoose.model("user",custSchema);
