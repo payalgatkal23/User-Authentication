6cd3bace66871bac8d1cee8874db1a7956f5ca4a
payalgatkal23 <payalgatkal50@gmail.com>
2026-04-29 01:24:19 +0530

    updated

--- server.js
+++ server.js
@@ -8,11 +8,3 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect("mongodb://localhost:27017/L_R_Info")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB Connection Error:", err));
-
-app.use("/", userRoutes);
-
-app.listen(5000, () => console.log("Server running on port 5000"));
