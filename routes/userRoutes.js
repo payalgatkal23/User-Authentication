d2fb47d576971da9a22c6ff908cd241736b76a14
payalgatkal23 <payalgatkal50@gmail.com>
2026-04-29 15:18:42 +0530

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/userController");
+const controller = require("../Controller/userController");
 
 router.post("/register", controller.registerUser);
 router.post("/login", controller.loginUser);
